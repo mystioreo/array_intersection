ad3ee9a6d95bef72910db79a44b769877dd404f9
Daniela Di Iorio <danielad@gmail.com>
2018-09-12 22:43:32 -0700

    authored array intersection

--- lib/array_intersection.rb
+++ lib/array_intersection.rb
@@ -1,4 +1,19 @@
+#space complexity O[n], time complexity O[n*m]
 # Creates a new array to return the intersection of the two input arrays
 def intersection(array1, array2)
-  raise NotImplementedError
+  i = 0
+  intersect = []
+  unless array1 == nil || array2 == nil
+    until array1[i] == nil
+      j = 0
+      until array2[j] == nil
+        if array1[i] == array2[j]
+          intersect << array1[i]
+        end
+        j += 1
+      end
+      i += 1
+    end
+  end
+  return intersect
 end
